f34592fdda768f5fc9ae9db70a347fe387f77e05
karthickwiz <37250088+karthickwiz@users.noreply.github.com>
2019-07-12 18:49:09 +0530

    Add files via upload

---
+++ xml/src/test/java/com/example/demo/XmlApplicationTests.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XmlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+	@Test
+	public void contextLoads1() {
+	}
+	@Test
+	public void contextLoads3() {
+	}
+	@Test
+	public void contextLoads2() {
+	}
+
+}
