3ef2d20ddea425d33863b2d0bb97f1c6f091d7fd
karthickwiz <37250088+karthickwiz@users.noreply.github.com>
2019-07-12 22:01:53 +0530

    Add files via upload

---
+++ xml/test.bat
@@ -0,0 +1,6 @@
+@echo off
+echo "HELLOO"
+
+cd C:\Program Files (x86)\Jenkins\workspace\e2eXML_master-LP3E5AAO3SWI67DQWYG54HSN27KQADINPZEWKQV6VHI6RZCPXLPQ\xml\build\test-results
+
+copy /b TESTS-TestSuitesE2E_CAAS.xml +,,
